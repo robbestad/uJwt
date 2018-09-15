58f723ba5550ed0c0c1d16d7c4f28d301cd145d0
Sven Anders Robbestad <robbestad@gmail.com>
2018-09-15 13:45:42 +0200

    Restructure repo

--- client/lib/decode.js
+++ client/lib/decode.js
@@ -1,22 +1,17 @@
 const Crypto = require('crypto');
 
-function DecodeSignature(key,queryString) {
+function DecodeSignature(key, queryString) {
 
 	const sharedSecret = key;
 
-// Get signature
 	const _ = queryString.split(".");
 	const query = Buffer.from(_[0], 'base64').toString('ascii'),
 		retrievedSignature = _[1];
 
-// Recalculate signature
 	const computedSignature = Crypto.createHmac('sha256', sharedSecret).update(_[0]).digest('base64');
-
-// Compare signatures
 	const computedSignatureBuffer = Buffer.from(computedSignature, 'base64');
 	const retrievedSignatureBuffer = Buffer.from(retrievedSignature, 'base64');
 
-// NOTE: might want to check length of buffers
 	try {
 		const valid = Crypto.timingSafeEqual(computedSignatureBuffer, retrievedSignatureBuffer);
 		if (valid) {
@@ -27,4 +22,5 @@ function DecodeSignature(key,queryString) {
 		console.log(e);
 	}
 }
-module.exports=DecodeSignature;
+
+module.exports = DecodeSignature;
