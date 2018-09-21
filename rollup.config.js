becd4141409a04fd4ba5b43cc2ff9f20dc765ce7
Sven Anders Robbestad <robbestad@gmail.com>
2018-09-21 10:44:09 +0200

    Add rollup minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import minify from 'rollup-plugin-babel-minify';
+
 export default {
- input: "index.js",
- external: ["crypto"],
- output: {
- file: 'bundle.js',
- 	format: "cjs"
- }
+	input:    "index.js",
+	external: ["crypto"],
+	plugins:  [minify({comments:false, banner:"/*openresty-jwt - a jwt implementation written explicitly for use with nginx and OpenResty*/"})],
+	output:   {
+		file:   'bundle.js',
+		format: "cjs"
+	}
 }
