698373682ffde791661bb71f1d14a58350bfddf1
Sven Anders Robbestad <robbestad@gmail.com>
2018-09-21 13:51:08 +0200

    Fix hours...

--- client/__tests__/add-hours.js
+++ client/__tests__/add-hours.js
@@ -4,11 +4,11 @@ describe('Add hours', () => {
 	it('Verifies that date-fnc adds 1 hour to current Date', () => {
 		const now = new Date();
 		const future = AddHours(now,1);
-		expect(future.getTime()).toBe(now.getTime()+1*60*60)
+		expect(future.getTime()).toBe(now.getTime()+1*60*60*1000)
 	});
-	it('Verifies that date-fnc adds 10 hour to current Date', () => {
+	it('Verifies that date-fnc adds 168 hour to current Date', () => {
 		const now = new Date();
-		const future = AddHours(now,10);
-		expect(future.getTime()).toBe(now.getTime()+10*60*60)
+		const future = AddHours(now,168);
+		expect(future.getTime()).toBe(now.getTime()+168*60*60*1000)
 	});
 })
--- client/lib/add-minutes.js
+++ client/lib/add-minutes.js
@@ -1,6 +1,6 @@
 function addMinutes(date, units) {
 	const _date = new Date(date);
-	_date.setTime(_date.getTime() + units * 60000);
+	_date.setTime(_date.getTime() + units * 60* 1000);
 	return _date;
 }
 export default addMinutes;
